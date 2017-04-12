cdd382d28b3acf13c260296717206a841f2a4894
light <light@mitake.com.tw>
2017-04-12 10:49:22 +0800

    after merge

--- src/com/light/remoteController/event/RenameEvent.java
+++ src/com/light/remoteController/event/RenameEvent.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.light.remoteController.event;
 
 import com.google.gson.JsonObject;
@@ -34,45 +34,3 @@ public class RenameEvent extends ActionEvent{
 			throw new OperationException("IlligalData").setRetMsg("IsAlreadySameNameInChannel");
 	}
 }
-=======
-package com.light.remoteController.event;
-
-import com.google.gson.JsonObject;
-import com.light.Exception.OperationException;
-import com.light.bean.Member;
-import com.light.channel.Channel;
-import com.light.channel.Channel.Callback;
-import com.light.channel.ChannelManager;
-import com.light.channel.ChannelManagerFactory;
-
-public class RenameEvent extends ActionEvent{
-	
-	static { isDependencyAction = false ; }
-	ChannelManager channelManager = ChannelManagerFactory.getChannelManagerInstance();
-	
-	protected JsonObject data   ;
-	protected Member 	 member ;
-	
-	public RenameEvent(Member member,JsonObject data){
-		this.member = member;
-		this.data = data;
-	}
-	
-	@Override
-	public void handdle() throws OperationException {
-		String  channelID = data.get("channelID").getAsString();
-		String  name 	  = data.get("messageBody").getAsJsonObject().get("data").getAsString();
-		Channel channel   = channelManager.findChannel(channelID);
-		Callback callback =new Callback(){
-			@Override
-			public Member findMemberByNameCallback(Member result) {
-				//If the name isn't used , so user can rename of it
-				if(result == null ) member.setMemberName(name);
-				return result;
-			}
-		};
-		if(channel.findMemberByName(name,callback) != null) 
-			throw new OperationException("IlligalData").setRetMsg("IsAlreadySameNameInChannel");
-	}
-}
->>>>>>> remote/master
