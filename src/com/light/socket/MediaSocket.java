ebe960212eba8722eb3d316a7c92b95cdc9d0416
light <codexkz@gmail.com>
2017-05-08 06:05:22 +0800

    stream create 2

--- src/com/light/socket/MediaSocket.java
+++ src/com/light/socket/MediaSocket.java
@@ -14,11 +14,13 @@ import com.light.bean.Member;
 public class MediaSocket extends BasicSocket {
 	
 	private static final long serialVersionUID = 1L;
+	private  final static  int BINARY_MESSAGE_BUFFER_SIZE = 300 * 1024 * 1024;  // 300 MB
 	
 	MediaSocketPair mediasockpair ;
 	
 	@Override
 	public void doAfterStart(){
+		this.session.setMaxBinaryMessageBufferSize(BINARY_MESSAGE_BUFFER_SIZE);
 		String mediasockettype = this.getSession().getPathParameters().get("mediasockettype");
 		String mediasockpairid = this.getSession().getPathParameters().get("mediasockpairid");
 		mediasockpair = this.channel.getMediaSocketPairMap().get(mediasockpairid);
@@ -52,10 +54,12 @@ public class MediaSocket extends BasicSocket {
 	
 	
 	@OnMessage
-	public void onMessage(Session userSession, ByteBuffer data ) {
+	public void onMessage(Session userSession, byte[]  data ) {
 		try{
+			System.out.println(this.member.getMemberUUID()+" is sending ByteBuffer : "+data.length);
+			mediasockpair.sender.session.getBasicRemote().sendBinary(ByteBuffer.wrap(data));
 			for(MediaSocket socket : mediasockpair.getReceiverList() ){
-				if (socket.session.isOpen())  socket.session.getBasicRemote().sendBinary(data);
+				if (socket.session.isOpen())  socket.session.getBasicRemote().sendBinary(ByteBuffer.wrap(data));
 			}
 		}catch(IOException e){
 			e.printStackTrace();
